c016ea8b2f67af82b34bacfadf49f4ad71ee923c
techmannih <manishchaudharyttt@gmail.com>
2026-06-03 02:32:19 +0530

    Export Ultra Librarian bridge client from the package root

--- index.ts
+++ index.ts
@@ -1,2 +1,17 @@
 export * from "./lib/kicad-archive";
 export * from "./lib/ti-parts-engine";
+export { createUltraLibrarianBridgeClient } from "./lib/ultra-librarian-bridge-client";
+export type {
+  BridgeFetch,
+  BridgeLogger,
+  DownloadKicadArchiveResponse,
+  JsonObject,
+  JsonPrimitive,
+  JsonValue,
+  KicadExportRequest,
+  SearchPartResult,
+  SearchPartsRequest,
+  SearchPartsResponse,
+  UltraLibrarianBridgeClient,
+  UltraLibrarianBridgeClientOptions,
+} from "./lib/ultra-librarian-bridge-client";
